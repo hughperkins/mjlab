6aef7a9c4362878b9bb753f21cf660952b9645d0
Hugh Perkins <hughperkins@gmail.com>
2026-01-09 15:07:52 -0500

    disable cuda graph

--- src/mjlab/sim/sim.py
+++ src/mjlab/sim/sim.py
@@ -131,6 +131,7 @@ class Simulation:
     self.use_cuda_graph = self.wp_device.is_cuda and wp.is_mempool_enabled(
       self.wp_device
     )
+    self.use_cuda_graph = False
     self.create_graph()
 
     self.nan_guard = NanGuard(cfg.nan_guard, self.num_envs, self._mj_model)
@@ -194,7 +195,7 @@ class Simulation:
   def step(self) -> None:
     with wp.ScopedDevice(self.wp_device):
       with self.nan_guard.watch(self.data):
-        if self.use_cuda_graph and self.step_graph is not None:
+        if self.use_cuda_graph and self.step_graph is not None and False:
           wp.capture_launch(self.step_graph)
         else:
           mjwarp.step(self.wp_model, self.wp_data)
